abe5d5594bc3eda7f92df04c44667bc7095c286a
corinneaars <corinne.aars@gmail.com>
2024-04-15 14:22:02 -0500

    remove message from index.js

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Layout>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Hello, click <Link href='/CanadianCustomers'>this link</Link> to see the query. I don't know why it's not working.</h1>
+        <h1>Hello, click <Link href='/CanadianCustomers'>this link</Link> to see the query.</h1>
         <Image
           src="/next.svg"
           alt="Next.js Logo"
