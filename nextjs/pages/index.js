c91429174a8d838631f9c4fc453dae225cd4f2cd
Rachel Koerner <rachel_koerner1@baylor.edu>
2024-04-17 14:15:47 -0500

    Index Images

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -6,18 +6,20 @@ export default function Home() {
   return (
     <Layout>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Hello, click <Link href='/CanadianCustomers'>this link</Link> to see the query.</h1>
+        <h1>Hello, click <Link href='/CanadianCustomers'>this link</Link> to see our totally awesome query.</h1>
         <Image
           src="/next.svg"
           alt="Next.js Logo"
           width={200}
           height={200}
+          class = "invert"
         />        
         <Image
           src="/vercel.svg"
           alt="Next.js Logo"
           width={200}
           height={200}
+          class = "invert"
         />
       </main>
 
