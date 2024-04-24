cb5b303eff3d8d0edfa0b8269913fb8c7acf105e
Rachel Koerner <rachel_koerner1@baylor.edu>
2024-04-24 00:55:38 -0500

    Home Page Updates
    Following template

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -20,10 +20,59 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <section className="relative">
+        <div className="px-4 pt-10 mx-auto max-w-7xl md:pt-16">
+          <div className="w-full pb-5 mx-auto text-center md:w-11/12">
+            <h1 className="mb-3 text-5xl font-bold tracking-tight text-gray-900 dark:text-gray-100 md:text-6xl">
+              Welcome to our
+              <br />
+              <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-green-500 via-indigo-500 to-purple-500 dark:from-green-500 dark:via-indigo-400 dark:to-purple-500">
+                VIDEO STORE
+              </span>
+            </h1>
+            <p className="max-w-xl pt-5 mx-auto text-lg text-gray-600 dark:text-gray-400 md:text-lg">
+              Our NextJS DVD Rental Store allows users to add new customers and assign video rentals.
+            </p>
+            <div className="mt-6 text-center md:ml-6">
+              <a
+                className="inline-flex items-center px-5 py-3 text-sm font-medium text-gray-300 transition duration-300 bg-black rounded hover:bg-gray-800 dark:hover:bg-gray-200 dark:text-gray-700 dark:bg-white"
+                aria-label="learn more"
+                rel="noreferrer"
+                href="https://github.com/minor/plutonium/"
+              >
+                <span className="flex justify-center">New Customer</span>
+              </a>
+              <br className="sm:hidden" />
+              <a
+                className="inline-flex items-center px-5 py-3 mt-2 ml-0 text-sm font-medium text-gray-700 transition duration-300 border rounded shadow dark:hover:border-gray-500 hover:shadow-md md:ml-2 dark:text-gray-300"
+                aria-label="learn more"
+              >
+                <span className="flex justify-center">Rent</span>
+              </a>
+            </div>
+          </div>
+          <div className="relative w-full py-10 mx-auto text-center md:py-32 md:my-12 md:w-10/12">
+            <div className="relative z-10">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://nextjstemplates.com/templates/plutonium"
+              >
+                <img
+                  className="transition duration-700 rounded-xl ring-1 ring-black ring-opacity-5 hover:transform hover:scale-105"
+                  src="/next.svg"
+                  alt="Placeholder Image"
+                  width="350"
+                />
+              </a>
+            </div>
+            <p className="z-10 my-8 text-sm font-medium text-gray-500">
+              :)
+            </p>
+          </div>
+        </div>
+        </section>
       </nav>
-      <main className="container py-5">
-        <h1>Welcome to our Video Store!</h1>
-      </main>
     </Layout>
   );
 }
